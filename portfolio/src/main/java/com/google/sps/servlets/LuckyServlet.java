4844ccd34b7483fd6c4728727542a8fa6166242a
Michael Kelly <cybernationnews@gmail.com>
2021-06-06 23:03:06 +0000

    Early implementation of fetch

--- portfolio/src/main/java/com/google/sps/servlets/LuckyServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LuckyServlet.java
@@ -1,5 +1,8 @@
 package com.google.sps.servlets;
 
+import java.util.Calendar;
+import java.lang.Math;
+import java.util.Random;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,12 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
-@WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
-
+@WebServlet("/lucky")
+public class LuckyServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Michael!</h1>");
+    response.getWriter().println(Math.abs(new Random().nextInt()));
   }
 }
